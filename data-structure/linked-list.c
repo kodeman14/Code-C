b5a3fd74b9270a4d80bf0cfccbcb2c60d86575e2
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-01-28 17:59:36 -0500

    Create linked-list.c

---
+++ data-structure/linked-list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+#include <alloc.h>
+
+#define newnode (struct node*)
+malloc (sizeof (struct node))
+
+// ordered set of data elements
+// each contains link to successor
+// length of linked list is calculated
+
+struct node {
+  int data;
+  struct node *next;
+};
+
+struct node *create_list();
+
+void main() {
+
+}
+
+struct node *create_list() {
+
+}
+
+int find_len (struct node *f) {
+
+}
