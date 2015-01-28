0d4a9ce8d761c27d405b877e495c478b92aa645a
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-01-27 22:50:24 -0500

    Create letter-count.c

---
+++ string/letter-count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char str[100];
+  int c = 0, count[26] = {0};
+  printf("enter a string\n");
+  gets(str);
+  while (str[c] != '\0') {
+    if (str[c] >= 'a' && str[c] <= 'z') {
+      count[str[c] - 'a']++;
+    } c++;
+  }
+  for (c=0; c<26; c++) {
+    if (count[c] != 0) {
+      printf("%c occurs %d times\n", c+'a', count[c]);
+    }
+  }
+  return 0;
+}
