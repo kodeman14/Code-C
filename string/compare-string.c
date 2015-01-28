d4298897fc19e26241537d1ac1a1ee8d8b0d4d09
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-01-27 22:37:42 -0500

    Create compare-string.c

---
+++ string/compare-string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char a[100], b[100];
+  printf("enter first string\n");
+  gets(a);
+  print("enter second string\n");
+  gets(b);
+  if (strcmp(a,b) == 0) {
+    printf("strings are equal\n");
+  } else {
+    printf("strings aren't equal\n");
+  }
+  return 0;
+}
