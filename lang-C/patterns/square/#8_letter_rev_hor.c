799ff566a3eaecc42e986493c4fe093383b83a66
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-08 19:29:56 -0400

    Create #8_letter_rev_hor.c

---
+++ lang-C/patterns/square/#8_letter_rev_hor.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  clrscr();
+  for (char i='E'; i>='A'; i--) {
+    for (int j='E'; j>='A'; j--) {
+      printf("%c", i);
+    } printf("\n");
+  }
+  getch();
+}
