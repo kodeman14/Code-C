6fe37364c56f8cad0ae6fd78b507e31b354684ba
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-08 19:22:25 -0400

    Create #7_num_rev_vert.c

---
+++ lang-C/patterns/square/#7_num_rev_vert.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  clrscr();
+  for (int i=5; i>=1; i--) {
+    for (int j=5; j>=1; j--) {
+      printf("%d", j);
+    } printf("\n");
+  }
+  getch();
+}
