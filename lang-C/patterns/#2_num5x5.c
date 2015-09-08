fd74b1129c39212f57b76002fa1a02d75287bcab
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-08 19:03:05 -0400

    Update and rename num5x5.c to #2_num5x5.c

---
+++ lang-C/patterns/#2_num5x5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  clrscr();
+  for (int i=1; i<=5; i++) {
+    for (int j=1; j<=5; j++) {
+      printf("%d", i); // can be any char
+    } printf("\n");
+  }
+  getch();
+}
--- lang-C/patterns/num5x5.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main() {
-  clrscr();
-  for (int i=1; i<=5; i++) {
-    for (
