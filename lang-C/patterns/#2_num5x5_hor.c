3342001f0f37f513b848bd16ed3d089594fdbaf3
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-08 19:04:21 -0400

    Update and rename #2_num5x5.c to #2_num5x5_hor.c

--- lang-C/patterns/#2_num5x5_hor.c
+++ lang-C/patterns/#2_num5x5_hor.c
@@ -5,7 +5,7 @@ void main() {
   clrscr();
   for (int i=1; i<=5; i++) {
     for (int j=1; j<=5; j++) {
-      printf("%d", i); // can be any char
+      printf("%d", i);
     } printf("\n");
   }
   getch();
