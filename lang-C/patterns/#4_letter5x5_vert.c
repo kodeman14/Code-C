67d204c7b9e04142669c77db556e0e304e5b19e5
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-08 19:07:35 -0400

    Create #4_letter5x5_vert.c

---
+++ lang-C/patterns/#4_letter5x5_vert.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  clrscr();
+  for (char i='A'; i<='E'; i++) {
+    for (char j='A'; j<='E'; j++) {
+      printf("%c", j);
+    } printf("\n");
+  }
+  getch();
+}
