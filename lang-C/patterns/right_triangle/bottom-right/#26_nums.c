e39baee0cd12c255b8789b23ede291145aa14086
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-09 10:13:05 -0400

    Create #26_nums.c

---
+++ lang-C/patterns/right_triangle/bottom-right/#26_nums.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  int z=1, n=5;
+  clrscr();
+  for (int i=1; i<=n; i++) {
+    for (int j=n-1; j>i; j--) {
+      printf(" ");
+    } for (int k=1; k<=z; k++) {
+        printf("%d", k);
+    } z++;  
+    printf("\n");
+  }
+  getch();
+}
+
+/*
+    1
+   12
+  123
+ 1234
+12345
+
+*/
