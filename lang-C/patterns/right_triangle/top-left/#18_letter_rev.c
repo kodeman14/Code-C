353dff1624526ca8c34fe3302d077b3f5bc58965
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-09 10:04:20 -0400

    Update #18_letter_rev.c

--- lang-C/patterns/right_triangle/top-left/#18_letter_rev.c
+++ lang-C/patterns/right_triangle/top-left/#18_letter_rev.c
@@ -5,7 +5,7 @@ void main() {
   clrscr();
   for (int i=0; i<5; i++) {
     for (int j=5; j>i; j--) {
-      printf("%c", i+65); //65 -> ascii of 'A'
+      printf("%c", i+65); // 65 -> ascii of 'A'
     } printf("\n");
   }
   getch();
