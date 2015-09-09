19b6214276b7225779ca82ee07842fa5c373672c
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-09 10:19:20 -0400

    Create #28_letters.c

---
+++ lang-C/patterns/right_triangle/bottom-right/#28_letters.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  int z=1, n=5;
+  clrscr();
+  for (int i=0; i<n; i++) {
+    for (int j=n-1; j>i; j--) {
+      printf(" ");
+    } for (int k=1; k<=z; k++) {
+        printf("%c", k+65); // 65 -> ascii of 'A'
+    } z++;  
+    printf("\n");
+  }
+  getch();
+}
+
+/*
+    A
+   AB
+  ABC
+ ABCD
+ABCDE
+
+*/
