b26481c1d64c208fed00c4f154ffe0c5912889db
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-08 20:13:42 -0400

    Create #16_num_rev.c

---
+++ lang-C/patterns/right_triangle/#16_num_rev.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  clrscr();
+  for (int i=1; i<=5; i++) {
+    for (int j=5; j>=1; j--) {
+      printf("%d", i);
+    } printf("\n"5;
+  }
+ getch();
+}
+
+/*
+
+11111
+2222
+333
+44
+5
+
+*/
