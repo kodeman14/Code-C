63b504dd9ca9d04c17fe86335b15f2e940492ebb
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-09 10:39:37 -0400

    Update #29_stars.c

--- lang-C/patterns/right_triangle/top-right/#29_stars.c
+++ lang-C/patterns/right_triangle/top-right/#29_stars.c
@@ -5,11 +5,11 @@ void main() {
   int z=5, n=5; // (z -> max stars) + (n -> size)
   clrscr();
   for (int i=1; i<=n; i++) {
-    for (int j=1; j<i; j++) {
+    for (int j=1; j<=i; j++) {
       printf(" ");
     } for (int k=1; k<=z; k++) {
         printf("*");
-    } z--;  
+    } z--;
     printf("\n");
   }
   getch();
