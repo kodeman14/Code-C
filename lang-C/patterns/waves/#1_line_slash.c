d26da4378cea2a4041c83e61efe8cc34696797e1
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-09 11:07:56 -0400

    Update #1_line_slash.c

--- lang-C/patterns/waves/#1_line_slash.c
+++ lang-C/patterns/waves/#1_line_slash.c
@@ -5,17 +5,19 @@ void main() {
   int e=2, wave_height=4, wave_length=4;
   // change value to increase/decrease height/length of wave
   int n, count, x=1; clrscr();
-  for (int j=wave_height; j<=wave_height+i; j++) {
-    printf(" ");
-  }
-  for (count=1; count<=wave_length; count++) {
-    for (n=(2*wave_height)-2; n>=x; n--) {
+  for (int i=0; i<wave_height; i++) {
+    for (int j=wave_height; j<=wave_height+i; j++) {
       printf(" ");
     }
-    for (int k=1; k<=e; k++) {
-      if (k==1) printf("/");
-      else if (k==e) printf("\");
-      else printf(" ");
+    for (count=1; count<=wave_length; count++) {
+      for (n=(2*wave_height)-2; n>=x; n--) {
+        printf(" ");
+      }
+      for (int k=1; k<=e; k++) {
+        if (k==1) printf("/");
+        else if (k==e) printf("\");
+        else printf(" ");
+      }
     }
     x+=2; e+=2;
     printf("\n");
