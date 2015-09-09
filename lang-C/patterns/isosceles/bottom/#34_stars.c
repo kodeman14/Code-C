60eddee8cffc3a37aef26dec1853902007f43d78
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-09 10:50:14 -0400

    Update #34_stars.c

--- lang-C/patterns/isosceles/bottom/#34_stars.c
+++ lang-C/patterns/isosceles/bottom/#34_stars.c
@@ -5,5 +5,30 @@ void main() {
   int min_stars=1, p_height=5;
   int p_space=p_height-1;
   clrscr();
-
+  
+  /*
+  change val of min_stars to set min # of *'s in pyramid
+  take odd # for odd # of *'s in each row, 1-3-5 etc
+  take even # for even # of *'s in each row, 2-4-6 etc
+  */
+  
+  for (int i=0; i<p_height; i++) {
+    for (int j=p_space; j>i; j--) {
+      printf(" ");
+    } for (int k=0; k<min_stars; k++) {
+        printf("*");
+    } min_stars+=2;
+    printf("\n");
+  }
+  getch();
 }
+
+/*
+
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
