9b989774a5dd3774363bc7848a7346907aaf688c
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2015-09-08 20:07:30 -0400

    Create #12_nums.c

---
+++ lang-C/patterns/right_triangle/#12_nums.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() {
+  clrscr();
+  for (int i=1; i<=5; i++) {
+    for (int j=1; j<=1; j++) {
+      printf("%d", j);
+    } printf("\n");
+  }
+  getch();
+}
+
+/*
+
+1
+12
+123
+1234
+12345
+
+*/
